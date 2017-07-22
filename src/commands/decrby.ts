1b8fc82bec6546dc11490f59bd44564691f97e2a
Nilesh Mali <nileshmali86@gmail.com>
2017-07-22 17:10:58 +0530

    feat(decrby): adds isNaN check for non integer value

--- src/commands/decrby.ts
+++ src/commands/decrby.ts
@@ -1,5 +1,8 @@
 export function decrby(key: string, decrement = '0') {
-  const curVal = Number(this.data.get(key));
+  const curVal = Number(this.data.get(key) || '0');
+  if (isNaN(curVal)) {
+    throw new Error(`Value for ${key} is not an integer.`);
+  }
   const nextVal = curVal - parseInt(decrement, 10);
   this.data.set(key, nextVal.toString());
   return nextVal;
