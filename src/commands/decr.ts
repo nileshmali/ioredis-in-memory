3c981649187c801ff33678953be662aecadfedf2
Nilesh Mali <nileshmali86@gmail.com>
2017-07-22 17:11:50 +0530

    feat(decr): removes duplicated code

--- src/commands/decr.ts
+++ src/commands/decr.ts
@@ -1,6 +1,3 @@
 export function decr(key: string) {
-  const curVal = Number(this.data.get(key));
-  const nextVal = curVal - 1;
-  this.data.set(key, nextVal.toString());
-  return nextVal;
+  return this.decrby(key, 1);
 }
