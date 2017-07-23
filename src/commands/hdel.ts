5b0b5fd43c93cc23e9cbcaf58e35acf9783785e2
Nilesh Mali <nileshmali86@gmail.com>
2017-07-23 21:48:50 +0530

    fix(hdel): add argument validation

--- src/commands/hdel.ts
+++ src/commands/hdel.ts
@@ -1,5 +1,8 @@
-export function hdel(key: string, ...fields: Array<any>) {
-  const value = this.data.get(key);
+export function hdel(key: string, ...fields: string[]) {
+  if (!key || !fields || fields.length === 0) {
+    throw new Error(`Wrong number of arguments.`);
+  }
+  const value = this.data.get(key) || {};
   const numDeleted = fields.filter((field) => {
     if ({}.hasOwnProperty.call(value, field)) {
       delete value[field];
