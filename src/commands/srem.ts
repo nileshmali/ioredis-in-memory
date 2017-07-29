4b51d60e36e94f1fbe8debdb5f7607fbc157a1ca
Nilesh Mali <nileshmali86@gmail.com>
2017-07-29 19:41:08 +0530

    fix(srem): throw if key does not contain a set

--- src/commands/srem.ts
+++ src/commands/srem.ts
@@ -1,4 +1,7 @@
 export function srem(key: string, ...values: Array<any>) {
+  if (this.data.has(key) && !(this.data.get(key) instanceof Set)) {
+    throw new Error(`Key ${key} does not contain a set`);
+  }
   let removed = 0;
   const set = this.data.get(key) || new Set();
   values.forEach((val) => {
