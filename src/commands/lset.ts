533c8b6a8f5f2ea299129e1f983c4647a660c1e7
Nilesh Mali <nileshmali86@gmail.com>
2017-07-29 19:39:19 +0530

    fix(lset): capitalize first letter in error message

--- src/commands/lset.ts
+++ src/commands/lset.ts
@@ -1,6 +1,6 @@
 export function lset(key: string, i: string, value: any) {
   if (!this.data.has(key)) {
-    throw new Error('no such key');
+    throw new Error('No such key');
   }
 
   if (this.data.has(key) && !(this.data.get(key) instanceof Array)) {
@@ -8,7 +8,7 @@ export function lset(key: string, i: string, value: any) {
   }
 
   const index = parseInt(i, 10);
-  const list = this.data.get(key) || [];
+  const list = this.data.get(key);
   list[index < 0 ? list.length + index : index] = value;
   this.data.set(key, list);
 
