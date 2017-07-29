9a54949da1d6f5b5278347c73411791dbc4a9104
Nilesh Mali <nileshmali86@gmail.com>
2017-07-29 19:40:37 +0530

    fix(sadd): throw if key does not contain a set

--- src/commands/sadd.ts
+++ src/commands/sadd.ts
@@ -1,6 +1,9 @@
 export function sadd(key: string, ...values: Array<any>) {
   let added = 0;
   const set = this.data.get(key) || new Set();
+  if (!(set instanceof Set)) {
+    throw new Error(`Key ${key} does not contain a set`);
+  }
   values.forEach((value) => {
     if (!set.has(value)) {
       added++;
