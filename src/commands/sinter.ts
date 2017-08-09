dd9449d781a57c369fde089159c02ad7ace6787a
Nilesh Mali <nileshmali86@gmail.com>
2017-08-09 17:03:48 +0530

    fix(sinter): corrects logic to compute intersection

--- src/commands/sinter.ts
+++ src/commands/sinter.ts
@@ -1,5 +1,5 @@
 export function sinter(...keys: Array<string>) {
-  const values: Array<any> = this.sunion(keys);
+  const values: Array<any> = this.sunion(...keys);
   const sets = keys.map(key => (this.data.get(key) || new Set()));
 
   const intersection = values.filter(value => (
