d80d0bc2be518f53455f3f8ce4fa9170c55e160d
Nilesh Mali <nileshmali86@gmail.com>
2017-08-09 17:06:39 +0530

    feat(strlen): adds validation

--- src/commands/strlen.ts
+++ src/commands/strlen.ts
@@ -1,3 +1,6 @@
 export function strlen(key: string) {
+  if (this.data.has(key) && typeof (this.data.get(key)) !== 'string') {
+    throw new Error(`Key ${key} does not contain a string`);
+  }
   return this.data.has(key) ? this.data.get(key).length : 0;
 }
