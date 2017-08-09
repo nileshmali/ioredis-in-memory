b747fbb461bc14926d851afaf1e4c0339cc1d72d
Nilesh Mali <nileshmali86@gmail.com>
2017-08-09 17:04:30 +0530

    feat(sismember): adds validation

--- src/commands/sismember.ts
+++ src/commands/sismember.ts
@@ -1,4 +1,7 @@
 export function sismember(key: string, value: any) {
   const set = this.data.get(key) || new Set();
+  if (!(set instanceof Set)) {
+    throw new Error(`Key ${key} does not contain a set`);
+  }
   return set.has(value) ? 1 : 0;
 }
