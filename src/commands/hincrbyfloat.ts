4b7f0c5b0a21a21c95febc446792bcb871a982fa
Nilesh Mali <nileshmali86@gmail.com>
2017-08-09 16:57:13 +0530

    feat(hincrbyfloat): adds validation

--- src/commands/hincrbyfloat.ts
+++ src/commands/hincrbyfloat.ts
@@ -1,9 +1,15 @@
 export function hincrbyfloat(key: string, field: string, increment: string) {
+  if (increment == null) {
+    throw new Error('Wrong number of arguments specified');
+  }
   const hash = this.data.get(key) || { [field]: '0' };
   if (!{}.hasOwnProperty.call(hash, field)) {
     hash[field] = '0';
   }
   const curVal = parseFloat(hash[field]);
+  if (isNaN(curVal)) {
+    throw new Error('Hash value is not a number');
+  }
   hash[field] = (curVal + parseFloat(increment)).toString();
   this.data.set(key, hash);
   return hash[field];
