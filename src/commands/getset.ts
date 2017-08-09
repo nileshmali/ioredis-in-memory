e30d3a2b1a2ba35b9232651c36d7e6404b09834b
Nilesh Mali <nileshmali86@gmail.com>
2017-08-09 16:59:07 +0530

    fix(getset): fix default value

--- src/commands/getset.ts
+++ src/commands/getset.ts
@@ -1,5 +1,5 @@
 export function getset(key: string, value: any) {
-  const old = this.data.get(key) || '';
+  const old = this.data.get(key) || null;
   this.data.set(key, value);
   this.expires.delete(key);
   return old;
