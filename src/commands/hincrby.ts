1729c60ed2e9077de63c286d455b58276c715cb3
Nilesh Mali <nileshmali86@gmail.com>
2017-08-09 11:20:41 +0530

    fix(hincrby): fix validations

--- src/commands/hincrby.ts
+++ src/commands/hincrby.ts
@@ -1,9 +1,15 @@
-export function hincrby(key: string, field: string, increment = '0') {
+export function hincrby(key: string, field: string, increment: string) {
+  if (increment == null) {
+    throw new Error('Wrong number of arguments specified');
+  }
   const hash = this.data.get(key) || { [field]: '0' };
   if (!{}.hasOwnProperty.call(hash, field)) {
     hash[field] = '0';
   }
   const curVal = Number(hash[field]);
+  if (isNaN(curVal)) {
+    throw new Error('Hash value is not an integer');
+  }
   const nextVal = curVal + parseInt(increment, 10);
   hash[field] = nextVal.toString();
   this.data.set(key, hash);
