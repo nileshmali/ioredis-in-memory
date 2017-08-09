cae59f098962f26f4749d80e5c334b878a9081fc
Nilesh Mali <nileshmali86@gmail.com>
2017-08-09 17:01:03 +0530

    fix(incrbyfloat): fixes default parameter value

--- src/commands/incrbyfloat.ts
+++ src/commands/incrbyfloat.ts
@@ -1,5 +1,8 @@
-export function incrbyfloat(key: string, increment: string) {
+export function incrbyfloat(key: string, increment = '0.0') {
   const curVal = parseFloat(this.data.get(key) || '0');
+  if (isNaN(curVal)) {
+    throw new Error(`Value for ${key} is not an integer.`);
+  }
   this.data.set(key, (curVal + parseFloat(increment)).toString());
   return this.data.get(key);
 }
