d69fe453ac58afb628b779d59784b16d081e8bc6
Nilesh Mali <nileshmali86@gmail.com>
2017-08-09 17:03:14 +0530

    fix(randomkey): correct logic to fetch key

--- src/commands/randomkey.ts
+++ src/commands/randomkey.ts
@@ -1,5 +1,8 @@
 import { random } from 'lodash';
 export function randomkey() {
-  const keys = this.data.keys();
+  const keys: string[] = [];
+  this.data.forEach((v: any, k: string) => {
+    keys.push(k);
+  });
   return keys.length > 0 ? keys[random(0, keys.length - 1)] : null;
 }
