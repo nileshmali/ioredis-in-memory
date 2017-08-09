db2f487611409b2cf8ff8b9a87c3225bf62fe44c
Nilesh Mali <nileshmali86@gmail.com>
2017-08-09 17:00:43 +0530

    feat(incrby): adds validation

--- src/commands/incrby.ts
+++ src/commands/incrby.ts
@@ -1,5 +1,8 @@
 export function incrby(key: string, increment = '0') {
   const curVal = Number(this.data.get(key) || '0');
+  if (isNaN(curVal)) {
+    throw new Error(`Value for ${key} is not an integer.`);
+  }
   const nextVal = curVal + parseInt(increment, 10);
   this.data.set(key, nextVal.toString());
   return nextVal;
